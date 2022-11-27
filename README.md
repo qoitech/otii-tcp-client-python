de38951c1008e10fdf0d5ffa1b1dba70de96ef8e
Joakim Kvarnlöf <joakim@kvarnlof.com>
2022-11-27 12:17:35 +0100

    Added otii_control.py for user management

--- README.md
+++ README.md
@@ -2,27 +2,37 @@
 
 The otii_tcp_client is a Python package for controlling the [Otii Automation Toolbox](https://www.qoitech.com/automation-toolbox/).
 
+New functionality in client v1.0.7 requiring [Otii software v3.1.0 or later](https://www.qoitech.com/download):
+
+- Added `otii_control.py` script for [user management](https://www.qoitech.com/docs/user-manual/automation-toolbox/user-management)
+
 New functionality in client v1.0.6 requiring [Otii software v3.1.0 or later](https://www.qoitech.com/download):
+
 - User management API (`otii.login`, `otii.logout`, `otii.get_licenses`, `otii.reserve_license` and `otii.return_license`)
 - `arc.get_channel_samplerate` command for Arc and Ace devices
 - `arc.set_channel_samplerate` command for Ace devices
 
 New functionality in client v1.0.5:
+
 - `get_devices` can now accept an optional `device_filter`
 
 New functionality in client v1.0.5 requiring [Otii software v3.0.0 or later](https://www.qoitech.com/download):
+
 - Statistics API (`get_channel_info`and `get_channel_statistics`)
 - `firmware_upgrade` command for Arc and Ace devices
 
 New functionality in client v1.0.4 requiring [Otii software v2.8.4 or later](https://www.qoitech.com/download):
+
 - New optional `timeout` parameter added to `otii.get_devices`, that specifies the time in seconds to wait for an avaliable device
 - Improved handling of TCP requests
 - Added `arc.get_main` command
 
 New functionality in client v1.0.3 requiring [Otii software v2.7.2 or later](https://www.qoitech.com/download):
+
 - `Recording` object now exposes the start time as `start_time`
 
 New functionality in client v1.0.2 requiring [Otii software v2.7.1 or later](https://www.qoitech.com/download):
+
 - `arc.[gs]et_src_cur_limit_enabled` commands
 - `arc.[gs]et_4wire` commands
 - `arc.enable_battery_profiling`, `arc.set_battery_profile` and `arc.wait_for_battery_data` commands
---
+++ otii_tcp_client/otii_control.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import argparse
+from otii_tcp_client import otii_connection, otii as otii_application, otii_exception
+
+def login(otii, username, password):
+    otii.login(username, password)
+
+def logout(otii):
+    otii.logout()
+
+def list_licenses(otii):
+    licenses = otii.get_licenses();
+    print(f'{"  Id"} {"Type":12} {"Reserved to":15} Hostname')
+    for license in licenses:
+        print(f'{license["id"]:4d} {license["type"]:12} {license["reservedTo"]:15} {license["hostname"]}')
+
+def reserve_license(otii, id):
+    otii.reserve_license(id)
+
+def return_license(otii, id):
+    otii.return_license(id)
+
+def main():
+    parser = argparse.ArgumentParser(description='Otii Control')
+    subparsers = parser.add_subparsers(dest='command', title='commands', required=True)
+
+    login_parser = subparsers.add_parser('login', help='Log in to Qoitech server')
+    login_parser.add_argument('-u', '--username', dest='username', required=True, help='Username')
+    login_parser.add_argument('-p', '--password', dest='password', required=True, help='Password')
+    subparsers.add_parser('logout', help='Log out from Qoitech server')
+    subparsers.add_parser('list-licenses', help='List all available licenses')
+    reserve_parser = subparsers.add_parser('reserve-license', help='Reserve license')
+    reserve_parser.add_argument('-i', '--id', dest='id', required=True, help='License id')
+    return_parser = subparsers.add_parser('return-license', help='Return license')
+    return_parser.add_argument('-i', '--id', dest='id', required=True, help='License id')
+    args = parser.parse_args()
+    command = args.command
+
+    connection = otii_connection.OtiiConnection('localhost', 1905)
+    connect_response = connection.connect_to_server()
+    if connect_response["type"] == "error":
+        print("Exit! Error code: " + connect_response["errorcode"] + ", Description: " + connect_response["payload"]["message"])
+        sys.exit()
+    otii = otii_application.Otii(connection)
+
+    if command == 'login':
+        login(otii, args.username, args.password)
+    elif command == 'logout':
+        logout(otii)
+    elif command == 'list-licenses':
+        list_licenses(otii)
+    elif command == 'reserve-license':
+        reserve_license(otii, int(args.id))
+    elif command == 'return-license':
+        return_license(otii, int(args.id))
+    else:
+        print(f'Unknown command {command}')
+
+    connection.close_connection()
+
+if __name__ == '__main__':
+    main()
