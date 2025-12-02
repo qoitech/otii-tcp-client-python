ecd00bf0be4946d3c205cbb4ab4f235942398ba4
Joakim Kvarnlöf <joakim@kvarnlof.com>
2025-12-02 13:59:04 +0100

    Updated import_log.py

--- examples/import_log.py
+++ examples/import_log.py
@@ -2,8 +2,13 @@
 '''
 Otii 3 Import log to an existing project
 
-This imports a text file with the following format:
-    timestamp;Log message
+This imports a csv file with the following format:
+    timestamp,message
+    0.0,[Starting line - setting up]
+    22.134,['Random Access Preamble'] ['L2 message'] ['UE --> SS']
+    22.134,['Random Access Response'] ['L2 message'] ['UE <-- SS']
+    22.254,['Description_none'] ['RRCConnectionRequest'] ['UE --> SS']
+    ...0
 
 If you want the script to login and reserve a license automatically
 add a configuration file called credentials.json in the current folder
@@ -15,26 +20,25 @@ using the following format:
     }
 
 '''
-import unicodedata
+import csv
 from otii_tcp_client import otii_client
 
-LOG_ID = 'Signaling'
+LOG_NAME = 'Signaling'
 LOG_FILE = 'signaling_trace.log'
 
 def import_log(otii: otii_client.Connect) -> None:
     ''' Import text log '''
     project = otii.get_active_project()
-    imported_log_id = project.create_user_log(LOG_ID)
+    log_id = project.create_user_log(LOG_NAME)
     recording = project.get_last_recording()
     assert recording is not None
 
-    with open(LOG_FILE, mode ='r', encoding='utf-8') as file:
-        lines = file.readlines()
-        for line in lines:
-            timestamp = float(line.split(';')[0])
-            columns = line.split(';')[1]
-            message = ''.join(col for col in columns if unicodedata.category(col)[0] != 'C')
-            recording.append_user_log(imported_log_id, timestamp, message)
+    with open(LOG_FILE, newline ='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            timestamp = float(row['timestamp'])
+            message = row['message']
+            recording.append_user_log(log_id, timestamp, message)
 
 def main() -> None:
     '''Connect to the Otii 3 application and run the measurement'''
